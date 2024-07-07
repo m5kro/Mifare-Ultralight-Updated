17b05db17c500f073f6d7bf879819a36c67035e0
m5kro <zhu.edric@gmail.com>
2024-07-08 00:10:23 +0800

    fix lint error 2

--- app/src/main/java/com/samsung/sprc/fileselector/EditTextCursorWatcher.java
+++ app/src/main/java/com/samsung/sprc/fileselector/EditTextCursorWatcher.java
@@ -1,32 +1,32 @@
 package com.samsung.sprc.fileselector;
 
-import android.widget.EditText;
 import android.content.Context;
 import android.util.AttributeSet;
+import androidx.appcompat.widget.AppCompatEditText;
 
-public class EditTextCursorWatcher extends EditText {
+public class EditTextCursorWatcher extends AppCompatEditText {
     public EditTextCursorWatcher(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
     }
-    
+
     public EditTextCursorWatcher(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
-    
+
     public EditTextCursorWatcher(Context context) {
         super(context);
     }
-    
+
     private OnSelChangedListener listener = null;
-    
+
     public interface OnSelChangedListener {
         void onSelChanged(int selStart, int selEnd);
     }
-    
+
     public void setOnSelectionChangedListener(OnSelChangedListener o) {
         listener = o;
     }
-    
+
     @Override
     protected void onSelectionChanged(int selStart, int selEnd) {
         super.onSelectionChanged(selStart, selEnd); // Call to the superclass method
