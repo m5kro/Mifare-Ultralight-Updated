315230033b38b9ca3dd39c4f7fa80a343af204cc
m5kro <zhu.edric@gmail.com>
2024-07-08 00:06:47 +0800

    fix lint error 1

--- app/src/main/java/com/samsung/sprc/fileselector/EditTextCursorWatcher.java
+++ app/src/main/java/com/samsung/sprc/fileselector/EditTextCursorWatcher.java
@@ -5,31 +5,31 @@ import android.content.Context;
 import android.util.AttributeSet;
 
 public class EditTextCursorWatcher extends EditText {
-	public EditTextCursorWatcher(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-	}
-	
-	public EditTextCursorWatcher(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
-	
-	public EditTextCursorWatcher(Context context) {
-		super(context);
-	}
-	
-	private OnSelChangedListener listener = null;
-	
-	public interface OnSelChangedListener {
-		public void onSelChanged(int selStart, int selEnd);
-	}
-	
-	public void setOnSelectionChangedListener(OnSelChangedListener o) {
-		listener = o;
-	}
-	
-	@Override
-	protected void onSelectionChanged(int selStart, int selEnd) {
-		if (listener != null) listener.onSelChanged(selStart, selEnd);
-	}
+    public EditTextCursorWatcher(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+    
+    public EditTextCursorWatcher(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    
+    public EditTextCursorWatcher(Context context) {
+        super(context);
+    }
+    
+    private OnSelChangedListener listener = null;
+    
+    public interface OnSelChangedListener {
+        void onSelChanged(int selStart, int selEnd);
+    }
+    
+    public void setOnSelectionChangedListener(OnSelChangedListener o) {
+        listener = o;
+    }
+    
+    @Override
+    protected void onSelectionChanged(int selStart, int selEnd) {
+        super.onSelectionChanged(selStart, selEnd); // Call to the superclass method
+        if (listener != null) listener.onSelChanged(selStart, selEnd);
+    }
 }
-
